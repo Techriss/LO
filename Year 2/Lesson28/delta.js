513722ea4cc601ee940f11755bafe604e6ea397f
Krzysztof Kowalski <kkwlski@gmail.com>
2023-02-03 08:40:31 +0100

    lesson28

---
+++ Year 2/Lesson28/delta.js
@@ -0,0 +1,15 @@
+function check() {
+    let a = parseInt(document.getElementById('a').value)
+    let b = parseInt(document.getElementById('a').value)
+    let c = parseInt(document.getElementById('a').value)
+
+    let delta = (b*b) - (4*a*c)
+    let text = ""
+
+    if (delta < 0) text = "brak miejsc zerowych"
+    else if (delta == 0) text = "1 miejsce zerowe"
+    else text = "2 miejsca zerowe"
+
+    document.getElementById('res').innerHTML = text
+
+}
