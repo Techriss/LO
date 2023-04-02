c589aae9da4cc9ce45a647491f87695664659749
Krzysztof Kowalski <kkwlski@gmail.com>
2023-04-02 20:03:28 +0200

    website

--- website/script.js
+++ website/script.js
@@ -1,1 +1,24 @@
-const rok = 2023
+function openNav() {
+    document.querySelector(".menu-overlay").style.width = "100%";
+    document.querySelector(".menu-overlay").style.opacity = "1";
+    document.querySelector(".menu-overlay").style.left = "0";
+}
+  
+function closeNav() {
+    document.querySelector(".menu-overlay").style.opacity = "0";
+    document.querySelector(".menu-overlay").style.width = "0%";
+    document.querySelector(".menu-overlay").style.left = "100%";
+}
+
+function scrollDown(target)
+{
+    document.getElementById(target).scrollIntoView
+    ({
+        behavior: 'smooth',
+    });
+}
+
+function setYear() {
+    const y = new Date().getFullYear();
+    document.getElementById('year').innerHTML = y;
+}
